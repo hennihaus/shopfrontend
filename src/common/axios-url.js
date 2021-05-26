9907573cc32033b41bdd063c1ecbc9e56f224ea3
hennihaus <jan-hendrik.hausner@outlook.com>
2021-05-26 13:13:04 +0200

    changed default port #patch

--- src/common/axios-url.js
+++ src/common/axios-url.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const backendHostname = process.env.REACT_APP_BACKEND_HOST || 'Localhost';
-const backendPort = process.env.REACT_APP_BACKEND_PORT || 3005;
-
 const instance = axios.create({
-    baseURL: `http://${backendHostname}:${backendPort}/`
+    baseURL: `http://localhost:4201/`
 });
 
 export default instance;
